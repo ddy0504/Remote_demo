6e283159e79d84c6243b2f24fcbb2a474fad8353
ddy0504 <ddy0504@qq.com>
2017-04-20 23:31:57 +0800

    增加Insert函数

--- program/c11_learn/sqlite_c_test/sqlite_test.cpp
+++ program/c11_learn/sqlite_c_test/sqlite_test.cpp
@@ -3,6 +3,8 @@
 #include "sqlite3.h"
 using namespace std;
 
+sqlite3 *db;
+
 static int callback(void *data, int argc, char **argv, char **azColName){
    int i;
    fprintf(stderr, "%s: ", (const char*)data);
@@ -14,12 +16,9 @@ static int callback(void *data, int argc, char **argv, char **azColName){
 }
 
 int Open(){
-   sqlite3 *db;
-   char *zErrMsg = 0;
+   //sqlite3 *db;
    int rc;
-   char *sql;
-   const char* data = "Callback function called";
-
+   
    /* Open database */
    rc = sqlite3_open("testDB.db", &db);
    if( rc ){
@@ -29,30 +28,65 @@ int Open(){
       fprintf(stderr, "Opened database successfully\n");
    }
 
+   return 0;
+}
+
+int Select(){
+
+   char *sql;
+   const char* data = "Callback function called";
+   char *zErrMsg = 0;
    /* Create SQL statement */
    sql = "SELECT * from COMPANY";
 
    /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+   int rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
    if( rc != SQLITE_OK ){
       fprintf(stderr, "SQL error: %s\n", zErrMsg);
       sqlite3_free(zErrMsg);
+      return 0;
    }else{
       fprintf(stdout, "Operation done successfully\n");
    }
-   sqlite3_close(db);
-   return 0;
+   return 1;
+}
+
+int Insert(){
+   char *sql;
+   char *zErrMsg = 0;
+   /* Create SQL statement */
+   sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
+         "VALUES (2, 'Allen', 25, 'Texas', 15000.00 ); "     \
+         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
+         "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );" \
+         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
+         "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+
+   /* Execute SQL statement */
+   int rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+   if( rc != SQLITE_OK ){
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+      return 0;
+   }else{
+      fprintf(stdout, "Records created successfully\n");
+   }
+   return 1;
 }
 
 int main(int argc, char* argv[])
 {
-   char *zErrMsg = 0;
    int ret;
-   char *sql;
-   const char* data = "Callback function called";
 
    ret = Open();
 
-   return 0;
+   ret = Select();
 
+   ret = Insert();
+
+   Select();
+
+   sqlite3_close(db);
+
+   return 0;
 }
